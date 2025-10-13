e20efca141f8c98b3f9ff508354cdfb522f03b72
theisaachome <65864160+theisaachome@users.noreply.github.com>
2025-10-13 21:56:25 +0800

    eshop-api-wit-spring-boot

---
+++ modern-api-eshop/src/main/java/com/isaachome/eshop/ModernApiEshopApplication.java
@@ -0,0 +1,13 @@
+package com.isaachome.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernApiEshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModernApiEshopApplication.class, args);
+    }
+
+}
---
+++ modern-api-eshop/src/test/java/com/isaachome/eshop/ModernApiEshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaachome.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernApiEshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
